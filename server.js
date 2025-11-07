d804c21eb0ff37c384cc97f48b44f94b63ac4fcd
imanomura <206794822+imanomura@users.noreply.github.com>
2025-11-07 09:16:33 +0900

    過去追加コード

--- server.js
+++ server.js
@@ -5,11 +5,21 @@ const app = new Hono();
 app.use('/*', serveStatic({ root: './public' }));
 
 // GETリクエストに対する処理
+//クエリーパラメーター
 app.get('/api', async (c) => {
+  const query = new URLSearchParams({
+    name: this.name,
+    rank: this.rank
+  });
   const name = c.req.query('name');
   const rank = c.req.query('rank');
   return c.json({ message: 'GET', query: { name, rank } });
 });
+// app.get('/api/:name/:rank', async (c) => {
+//   const name = c.req.param('name');
+//   const rank = c.req.param('rank');
+//   return c.json({ message: 'GET', param: { name, rank } });
+// });
 
 app.post('/api', async () => {
   return c.json({ message: 'POST' });
